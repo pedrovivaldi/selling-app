fad6811f99aa174d70c1df555f248b636f16a237
Pedro Vivaldi <pedro.cotta.vivaldi@gmail.com>
2021-05-23 02:12:12 -0300

    feat: add badge to cart icon

--- packages/frontend/src/components/NavigationCart.js
+++ packages/frontend/src/components/NavigationCart.js
@@ -1,23 +1,38 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import { Badge } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+
 import { gStyle } from '../constants';
 
 import SvgCart from './icons/Svg.Cart';
 
-const NavigationCart = ({ navigation }) => (
-  <TouchableOpacity
-    accessible
-    accessibilityLabel="cart"
-    accessibilityComponentType="button"
-    accessibilityTraits="button"
-    activeOpacity={gStyle.activeOpacity}
-    onPress={() => navigation.navigate('Cart')}
-    style={{ paddingHorizontal: 16, paddingVertical: 8 }}
-  >
-    <SvgCart active />
-  </TouchableOpacity>
-);
+const NavigationCart = ({ navigation }) => {
+  const cart = useSelector((state) => state);
+
+  return (
+    <TouchableOpacity
+      accessible
+      accessibilityLabel="cart"
+      accessibilityComponentType="button"
+      accessibilityTraits="button"
+      activeOpacity={gStyle.activeOpacity}
+      onPress={() => navigation.navigate('Cart')}
+      style={{ paddingHorizontal: 16, paddingVertical: 8 }}
+    >
+      <SvgCart active />
+      {cart.length > 0 ? (
+        <Badge
+          value={cart.length}
+          containerStyle={{ position: 'absolute', top: -4, right: 0 }}
+        />
+      ) : (
+        <View></View>
+      )}
+    </TouchableOpacity>
+  );
+};
 
 NavigationCart.propTypes = {
   navigation: PropTypes.object.isRequired
